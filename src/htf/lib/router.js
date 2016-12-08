06948c9053c76f00f0fcf96984d6a4749a36c5fc
Rival-Thompson <s083731@ap.be>
2016-12-08 09:55:39 +0100

    Init

---
+++ src/htf/lib/router.js
@@ -0,0 +1,12 @@
+/**
+ * Created by rival on 2/10/2016.
+ */
+Router.configure({
+    layoutTemplate: 'layout',
+    loadingTemplate: 'loading',
+    notFoundTemplate: 'pagenotfound'
+});
+
+Router.route("/", {
+    name: "homeIndex"
+});
