18cdf6f80b5f090b56fd44fd270043cff37bbd29
Rival-Thompson <s083731@ap.be>
2016-12-08 11:01:29 +0100

    auto update every sec

--- src/htf/client/templates/index.js
+++ src/htf/client/templates/index.js
@@ -4,6 +4,8 @@
 
 let Rovers = [];
 RoversTracker = new Tracker.Dependency;
+let DataReqLoop;
+
 Template.homeIndex.helpers({
     rovers: function () {
         RoversTracker.depend();
@@ -12,6 +14,10 @@ Template.homeIndex.helpers({
 
 });
 Template.homeIndex.events({
+    'click #'(event){
+
+        clearInterval(DataReqLoop);
+    }
 
 });
 Template.homeIndex.rendered = function () {
@@ -23,8 +29,11 @@ Template.homeIndex.rendered = function () {
                     if(err2){
                         console.log("rover: " +ell +" offline");
                     }else{
+                        let distX = Math.abs(200-res2.data.position.x);
+                        let distY = Math.abs(200-res2.data.position.y);
+                        let dist = Math.floor( Math.sqrt(Math.pow(distX,2)+Math.pow(distY,2)));
                         console.log(res2.data);
-                        Rovers.push({id:ell,name:res2.data.name,direction:res2.data.direction,speed:res2.data.speed,position:res2.data.position})
+                        Rovers.push({id:ell,name:res2.data.name,direction:res2.data.direction,speed:res2.data.speed,position:res2.data.position,distance:dist});
                         RoversTracker.changed();
                     }
                 });
@@ -33,4 +42,27 @@ Template.homeIndex.rendered = function () {
 
         }
     });
+    DataReqLoop = setInterval(function () {
+        HTTP.call("GET", "https://roguerovers-api-develop.azurewebsites.net/api/channel",function (err,res) {
+            if(!err){
+                console.log(res.data);
+                res.data.forEach(function (ell,index) {
+                    HTTP.call("GET","https://roguerovers-api-develop.azurewebsites.net/api/channel/"+ell,function (err2,res2) {
+                        if(err2){
+                            console.log("rover: " +ell +" offline");
+                        }else{
+                            let distX = Math.abs(200-res2.data.position.x);
+                            let distY = Math.abs(200-res2.data.position.y);
+                            let dist = Math.floor( Math.sqrt(Math.pow(distX,2)+Math.pow(distY,2)));
+                            console.log(res2.data);
+                            Rovers.push({id:ell,name:res2.data.name,direction:res2.data.direction,speed:res2.data.speed,position:res2.data.position,distance:dist});
+                            RoversTracker.changed();
+                        }
+                    });
+                });
+                console.log(Rovers);
+
+            }
+        });
+    },1000);
 };
